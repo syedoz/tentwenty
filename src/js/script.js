82a23be58756864c0cad3942353fa26485abc675
Syed Moiz <moizali_2005@hotmail.com>
2018-02-03 05:58:26 +0400

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,29 @@
 $( document ).ready(function() {
+	var slidr = $('.banner-slider');
+	slidr.owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:false,
+	    items: 1
+	})
+	$('#res').click(function(){
+		slidr.trigger('owl.goTo', 2)
+		//alert('yehla')
+	});
+
+	var wrapperMenu = document.querySelector('.wrapper-menu');
+
+	wrapperMenu.addEventListener('click', function(){
+	  wrapperMenu.classList.toggle('open');
+	  if($(this).hasClass('open')){
+	  	$('.navItems').stop().animate({
+			left:'0px'
+		})	
+	  }
+	  else {
+	  	$('.navItems').stop().animate({
+			left:'-999px'
+		})
+	  }
+	})
 });
